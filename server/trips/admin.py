4aa0b78e5fc8ceab5caef13c84cd892fae69e956
Boya Jiang <jiang.boya@yahoo.com>
2016-03-15 19:35:31 -0700

    rolling back

--- server/trips/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from trips.models import User
-
-class TripsAdmin(admin.ModelAdmin):
-    fieldsets = [('User',{'fields': ['facebook_id','name','picUrl']}),]
-
-admin.site.register(User, TripsAdmin)
