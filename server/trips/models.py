5500bede5738d042d73d3e5c4e51312839d445e9
Boya Jiang <jiang.boya@yahoo.com>
2016-03-15 17:04:35 -0700

    correct a typo in schema

--- server/trips/models.py
+++ server/trips/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  facebook_id = models.CharField(max_length=100)
+  name = models.CharField(max_length=100)
+  picUrl = models.CharField(max_length=100)
+
+
+
+
